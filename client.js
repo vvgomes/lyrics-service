899e90bdcacb00d28eb5586029374aecf4481de8
Netto <nettofarah@gmail.com>
2011-07-14 11:54:06 -0300

    removing trash

--- client.js
+++ client.js
@@ -2,5 +2,3 @@ var musicData = $('.active').last().children('a[class^="queueSong"]');
 var song = musicData[0].text;
 var artist = musicData[1].text;
 console.log(song, artist);
-
-alert(song + ' - ' + artist);
