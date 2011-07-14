cce70ff8fb4fd94a4dac45f9a7d63e7393b2740f
Ivayr Netto <netto@netto-Latitude-E6510.(none)>
2011-07-14 15:56:11 -0300

    wrapping the music search in the server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var URL = require('url');
+var musicInfoService = require('./lib/music-info');
+
+var valid = function(query){
+	return query.artist && query.music;
+};
+
 var server = require('http').createServer(function(request, response) {
 	var url = URL.parse(request.url, true);
 	if (url.pathname != '/') return;
 	
-	response.writeHead(200, { 'Content-Type': 'application/json' });
-	response.end(JSON.stringify(url.query));
+	if (!valid(url.query)) {
+		response.writeHead(500);
+		response.end('Invalid Input. Expected: ?music=Music Name  &artist=Artist');	
+	};
+
+	musicInfoService(url.query).get(function(result){
+		response.writeHead(200, { 'Content-Type': 'application/json' });
+		response.end(JSON.stringify(result));	
+	});
+
+	
 }).listen(8080);
