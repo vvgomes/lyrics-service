cfbd2b455bf59f23f0eb3e85b633a4a8d02ad39f
Netto <nettofarah@gmail.com>
2011-07-15 17:36:40 -0300

    veryfing empty params

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ var URL = require('url');
 var musicInfoService = require('./lib/music-info');
 
 var valid = function(query){
-	return query.artist && query.music;
+	var notEmpty = query.artist != '' && query.music != '';
+	var notNull =  query.artist != null && query.music != null;
+	return notEmpty && notNull;
 };
 
 var server = require('http').createServer(function(request, response) {
 	var url = URL.parse(request.url, true);
 	if (url.pathname != '/') return;
-	
+
 	if (!valid(url.query)) {
 		response.writeHead(500);
 		response.end('Invalid Input. Expected: ?music=Music Name  &artist=Artist');	
-	};
+		return;
+	}
 
 	musicInfoService(url.query).get(function(result){
 		response.writeHead(200, { 'Content-Type': 'application/x-javascript' });
